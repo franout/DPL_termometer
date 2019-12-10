6535204ae48ca81db4796f140010e4ca40ce9b01
Candi-wu <57670622+Candi-wu@users.noreply.github.com>
2019-12-10 10:18:23 +0100

    Update clock_enable.vhdl

--- vhdl/interface_rf/clock_enable.vhdl
+++ vhdl/interface_rf/clock_enable.vhdl
@@ -11,12 +11,14 @@ signal cnt: natural range 0 to 999999 :=0;
 begin
 clk1ms: process(clk)
 begin
-	if(cnt= 999999) then
-		clk_1ms<='1';
-		cnt<=0;
-	else
-		clk_1ms<='0';
-		cnt<=cnt+1;
+	if(rising_edge(clk)) then 
+		if(cnt= 999999) then
+			clk_1ms<='1';
+			cnt<=0;
+		else
+			clk_1ms<='0';
+			cnt<=cnt+1;
+		end if;
 	end if;
 end process;
 end beh;
