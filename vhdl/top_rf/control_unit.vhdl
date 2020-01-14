f750778b893bbe6a11b74a5a20d0cf93ca253738
Candi-wu <57670622+Candi-wu@users.noreply.github.com>
2020-01-14 09:11:57 +0100

    Add files via upload

---
+++ vhdl/top_rf/control_unit.vhdl
@@ -0,0 +1,209 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    16:34:21 11/19/2019 
+-- Design Name: 
+-- Module Name:    control_unit - Behavioral 
+-- Project Name:  
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.math_real."log2";
+use IEEE.math_real."ceil";
+
+
+entity control_unit is
+GENERIC ( WATCH_DOG_COUNT: integer:= 100);
+PORT ( reset,clk: IN std_logic;
+-- selecting the indoor or outdoor temp sensor
+in_out_sel: IN std_logic;
+
+init_set_up: OUT std_logic ; -- commond signal to all component for setting up the lcd and sensors
+
+-----------------------------------------------------------------------------------------------------------------------
+-- done signals for each and every component will be also used for understanding that we are out of the set up phase ---
+------------------------------------------------------------------------------------------------------------------------
+
+in_out:OUT std_logic; -- common signal to all component for indoor outdoor sensor
+-- from/to comparison 
+start_comparison : OUT std_logic;
+done_comparison: IN std_logic;
+select_data: OUT std_logic_vector(1 DOWNTO 0); -- selecting between max,min and curr tmp 
+-- from/to lcd interface
+display: OUT std_logic;
+done_lcd: IN std_logic;
+-- from/to sensor interface
+start_meas: OUT std_logic;
+done_meas:IN std_logic;
+reset_i: OUT std_logic; -- internal reset for all interfaces 
+ready: OUT std_logic; -- switch on an led for notifyinh that the system is operative
+locked_clock: IN std_logic; -- notify from pll that clock speed has been reached
+-- to humidity sensor interface
+enable_humidity_sensor: OUT std_logic
+
+);
+end entity control_unit;
+
+
+architecture Behavioral of control_unit is
+type  state_t is (pll_lock_clock,set_up,set_up_hang,idle,measure_tmp,compute_max_min,display_curr_tmp,display_max_tmp,display_min_tmp);
+
+SIGNAL curr_state,next_state: state_t;
+SIGNAL curr_in_out_val,next_in_out_val: std_logic:='0';
+SIGNAL tc_wd,enable_wd: std_logic;
+
+-- component declaration it works like a watchdog timer for the setup phase and the idle period ( for refreshing the temperature) 
+
+
+component counter_wd is
+generic ( N : integer := 8;
+			MAX_VAL: integer :=255);
+    Port ( clk : in  STD_LOGIC;
+	 				enable: in std_logic;
+           reset : in  STD_LOGIC;
+           tc : out  STD_LOGIC);
+end component counter_wd;
+
+signal edge_detect : std_logic_vector( 1 downto 0 );
+
+begin
+
+
+watch_dog: counter_wd GENERIC MAP (N=> 1+integer(ceil(log2(real(WATCH_DOG_COUNT)))),MAX_VAL=>WATCH_DOG_COUNT) PORT MAP(enable=>enable_wd,clk=>clk,reset=>reset,tc=> tc_wd);
+
+
+regs:PROCESS(clk,reset)
+BEGIN
+IF (reset='1') THEN
+curr_state<=pll_lock_clock;
+curr_in_out_val<='0';
+ELSE 
+	IF(clk='1' AND clk'EVENT) THEN
+	    edge_detect <= edge_detect(0) &in_out_sel ;
+	curr_state<=next_state;
+	curr_in_out_val<=next_in_out_val;
+	END IF;
+
+END IF;
+END PROCESS regs;
+
+
+comb_logic:PROCESS(locked_clock,in_out_sel,done_comparison,done_lcd,done_meas,curr_state,tc_wd,edge_detect,curr_in_out_val)
+BEGIN
+-- default assignments of all signal 
+next_state<=curr_state;
+start_meas<='0';
+display<='0';
+select_data<="00";
+next_in_out_val<=curr_in_out_val;
+start_comparison<='0';
+init_set_up<='0';
+enable_wd<='0';
+reset_i<='0';
+ready<='1';
+in_out<=curr_in_out_val;
+enable_humidity_sensor<='1';
+CASE curr_state IS
+WHEN pll_lock_clock=> 	enable_humidity_sensor<='0';
+						ready<='0';
+						enable_wd<='1';
+						IF (locked_clock='1') THEN 
+						-- desired frequnecy reached
+						next_state<=set_up;
+						ELSE
+						next_state<=curr_state;
+						END IF;
+WHEN set_up=> init_set_up<='1';
+				enable_wd<='1';
+				enable_humidity_sensor<='0';
+			ready<='0';
+			-- interfaces will maintain the done signals up ( if they have completed the initialization ) as soon as the init_set-up remains at 1
+			IF ( done_lcd='1' ) THEN
+			next_state<=idle;
+			ELSIF ( tc_wd='1' ) THEN 
+			next_state<=set_up_hang;
+			ELSE
+			next_state<=curr_state;
+			END IF;
+WHEN set_up_hang=> -- tear downt the initialization signal for one clock cycle
+					enable_humidity_sensor<='0';
+					ready<='0';
+					reset_i<='1';
+					next_state<=set_up;
+WHEN idle=> enable_wd<='1';
+				IF( edge_detect="01" ) THEN
+				-- rising edge  INDOOR
+				next_state<=measure_tmp;
+				next_in_out_val<=in_out_sel; -- keeping constant until next idle period
+				ELSIF (edge_detect="10") THEN 
+				-- falling edge outdoor
+				next_state<=measure_tmp;
+				next_in_out_val<=in_out_sel; -- keeping constant until next idle period	
+				ELSIF ( tc_wd='1') THEN 
+				next_state<=measure_tmp;
+				next_in_out_val<=in_out_sel; -- keeping constant until next idle period
+				ELSE 
+				next_state<=curr_state;
+				END IF;
+WHEN measure_tmp=>
+				start_meas<='1';
+				IF(done_meas='1')THEN
+				next_state<=compute_max_min;
+				ELSE 
+				next_state<=curr_state;
+				END IF;			
+WHEN compute_max_min=>
+						start_comparison<='1';
+						IF(done_comparison='1') THEN
+						next_state<=display_max_tmp;
+						ELSE
+						next_state<=curr_state;
+						END IF;
+WHEN display_max_tmp=>
+							select_data<="01";
+							display<='1';
+							IF ( done_lcd ='1') THEN
+							next_state<=display_min_tmp;
+							ELSE 
+							next_state<=curr_state;
+							END IF;
+WHEN display_min_tmp=>select_data<="10";
+							display<='1';
+							IF ( done_lcd ='1') THEN
+							next_state<=display_curr_tmp;
+							ELSE 
+							next_state<=curr_state;
+							END IF;
+
+WHEN display_curr_tmp=> 
+							select_data<="11";
+							display<='1';
+							IF ( done_lcd ='1') THEN
+							next_state<=idle;
+							ELSE 
+							next_state<=curr_state;
+							END IF;
+
+-- for a safe fsm
+WHEN OTHERS=> 
+	next_state<=set_up;
+	start_meas<='0';display<='0';select_data<="00";in_out<='0';start_comparison<='0';init_set_up<='0';enable_wd<='0';
+	enable_humidity_sensor<='0';
+reset_i<='0';
+END CASE;
+
+END PROCESS comb_logic;
+
+end Behavioral;
+
---
+++ vhdl/top_rf/top_termometer.vhdl
@@ -0,0 +1,222 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    16:26:01 11/19/2019 
+-- Design Name: 
+-- Module Name:    top_termomemter - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+
+entity top_termometer is
+    Port ( reset : in  STD_LOGIC;
+           main_clk : in  STD_LOGIC;
+		   ind_outd_sw: in STD_LOGIC;
+           tmp_sensor : inout  STD_LOGIC;
+		   data_recei : in STD_LOGIC;
+           lcd_enable : out  STD_LOGIC;
+           lcd_rw : out  STD_LOGIC;
+           lcd_rs : out  STD_LOGIC;
+           lcd_data : out  STD_LOGIC_VECTOR (7 downto 0);
+				system_ready: out std_logic; -- signal to an led on the board
+				pwm_out: out std_logic;
+				sclk: inout std_logic;
+				sdata: inout std_logic
+				);
+end top_termometer;
+
+architecture structural of top_termometer is
+ 
+ 
+ 
+ 
+ component clock_generator_pll
+port
+ (-- Clock in ports  100 Mhz
+  CLK_IN1           : in    std_logic;
+  -- Clock out ports  10 Mhz
+  CLK_OUT1          : out    std_logic;
+  -- Status and control signals
+  RESET             : in     std_logic;
+    LOCKED            : out    std_logic
+
+ );
+end component;
+ 
+ 
+ -- main control unit
+ COMPONENT control_unit is
+GENERIC ( WATCH_DOG_COUNT: integer:= 100);
+PORT ( reset,clk: IN std_logic;
+-- selecting the indoor or outdoor temp sensor
+in_out_sel: IN std_logic;
+
+init_set_up: OUT std_logic ; -- commond signal to all component for setting up the lcd and sensors
+
+-----------------------------------------------------------------------------------------------------------------------
+-- done signals for each and every component will be also used for understanding that we are out of the set up phase ---
+------------------------------------------------------------------------------------------------------------------------
+
+in_out:OUT std_logic; -- common signal to all component for indoor outdoor sensor
+-- from/to comparison 
+start_comparison : OUT std_logic;
+done_comparison: IN std_logic;
+select_data: OUT std_logic_vector(1 DOWNTO 0); -- selecting between max,min and curr tmp 
+-- from/to lcd interface
+display: OUT std_logic;
+done_lcd: IN std_logic;
+-- from/to sensor interface
+start_meas: OUT std_logic;
+done_meas:IN std_logic;
+reset_i: OUT std_logic; -- internal reset for all interfaces 
+ready: OUT std_logic; -- switch on an led for notifyinh that the system is operative
+locked_clock: IN std_logic ; -- notify from pll that clock speed has been reached
+-- to humidity sensor interface
+enable_humidity_sensor: OUT std_logic
+);
+end COMPONENT control_unit;
+
+ 
+ 
+ 
+ -- datapath ( interfaces have its own CU ) 
+ -- comparison 
+
+component comparison is
+GENERIC (N:integer:= 8);
+    Port ( clk,reset: in STD_LOGIC;
+				in_out_sel: in std_logic;
+				data_in : in  STD_LOGIC_VECTOR (N-1 downto 0);
+           data_out : out  STD_LOGIC_VECTOR (N-1 downto 0);
+           start_comparison : in  STD_LOGIC; -- aka enable 
+           done_comparison : out  STD_LOGIC;
+           select_data : in  STD_LOGIC_VECTOR (1 downto 0));
+end component comparison; 
+
+
+-- termometer interfaces
+
+component temall is
+port	(clk : in std_logic;
+		 nrst:in std_logic;
+		 in_out: in std_logic;
+		 data: out std_logic_vector(8 downto 0);
+		 dq:  inout std_logic;            --ja[1]
+		 data_recei :in std_logic;        --ja[2]  receive from RF
+		 finish:	out std_logic
+		 );	
+end component temall;
+
+-- lcd interfaces 
+
+COMPONENT top_display is
+GENERIC(n: INTEGER := 9);
+PORT(clk,reset,start_lcd,init_set_up,ind_outd_select: in std_logic;
+		data_in: in std_logic_vector(N-1 DOWNTO 0);
+		done_lcd: out std_logic;
+		-- for lcd
+		data_out: OUT std_logic_vector(N-2 downto 0);
+		rs,r_w,enable: out std_logic
+	);
+
+end  COMPONENT top_display;
+
+
+-- humidity sensor interface ( i2c internal ) 
+COMPONENT  top_humidity_sensors_interface is
+    Port ( clk : in  STD_LOGIC;
+           reset : in  STD_LOGIC;
+           enable : in  STD_LOGIC;
+           pwm_out : out  STD_LOGIC;
+           sclk : inout  STD_LOGIC;
+           sdata : inout  STD_LOGIC);
+end  COMPONENT top_humidity_sensors_interface;
+
+
+
+SIGNAL reset_top,start_display,clk,locked_clock,reset_start_tmp,init_set_up,
+			in_out_sel ,start_comparison,done_display,done_comparison,done_meas,
+			enable_humidity_sensor,start_meas,reset_i: std_logic;
+SIGNAL select_data_comparison: std_logic_vector(1 DOWNTO 0);
+SIGNAL data_from_comparison,data_from_tmp_interface: std_logic_vector(8 DOWNTO 0);
+
+begin
+
+reset_top<= reset or reset_i;
+-- interconnecting components
+interface_lcd: top_display GENERIC MAP(9) PORT MAP(clk=>clk,reset=>reset_top,start_lcd=>start_display, 
+									init_set_up=>init_set_up, ind_outd_select=>in_out_sel,
+									data_in=>data_from_comparison,
+									done_lcd=>done_display,
+									data_out=>lcd_data,
+									enable=>lcd_enable,
+									r_w=>lcd_rw,
+									rs=>lcd_rs );
+interface_tmp_sensor: temall PORT MAP(clk=>main_clk,
+														in_out=>in_out_sel,
+														finish=>done_meas,             --这里是完成好的信号传输的地方
+														data=>data_from_tmp_interface,
+														dq=> tmp_sensor,
+														data_recei=> data_recei,
+														nrst=>reset_start_tmp);
+comparison_block:comparison GENERIC MAP (9) PORT MAP(clk=>clk,reset=>reset_top,
+				in_out_sel=>in_out_sel,
+				data_in=>data_from_tmp_interface ,
+           data_out=>data_from_comparison ,
+           start_comparison=>start_comparison ,
+           done_comparison=>done_comparison ,
+           select_data =>select_data_comparison);
+cu: control_unit GENERIC MAP (300) PORT MAP(locked_clock=>locked_clock,clk=>clk,reset=>reset,
+									in_out_sel=>ind_outd_sw,
+									init_set_up=> init_set_up,
+									in_out=>in_out_sel,
+									start_comparison=> start_comparison,
+									done_comparison=> done_comparison,
+									select_data=>select_data_comparison, 
+									display=>start_display,
+									done_lcd=>done_display,
+									start_meas=>start_meas,
+									done_meas=>done_meas ,
+									reset_i=>reset_i,
+									ready=> system_ready,
+									enable_humidity_sensor=> enable_humidity_sensor);
+									
+									
+interface_humidity_sensor:  top_humidity_sensors_interface PORT MAP(clk =>clk,
+           reset => reset_top,
+           enable =>enable_humidity_sensor ,
+           pwm_out => pwm_out,
+           sclk =>sclk,
+           sdata => sdata);
+			  
+			  
+reset_start_tmp<=not(start_meas or reset_top);
+
+-- pll 
+clk_gen : clock_generator_pll   port map    ( CLK_IN1 => main_clk,
+    -- Clock out ports
+    CLK_OUT1 => clk,
+    -- Status and control signals
+    RESET  => RESET,
+	 locked=> locked_clock
+    );
+	 
+-- additional components 
+
+
+
+end structural;
+
